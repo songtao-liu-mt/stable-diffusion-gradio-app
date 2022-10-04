d76df9d544c14e98828a731cb39e493e5901f949
Ubuntu <ubuntu@ip-172-31-3-97.ap-northeast-1.compute.internal>
2022-10-04 01:13:47 +0000

    fix the python command path

--- auto_run.sh
+++ auto_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 conda activate pytorch
 cd /home/ubuntu/stable-diffusion-gradio-app
-nohup python app.py > logger.log 2>&1 &
+nohup /opt/conda/envs/pytorch/bin/python app.py > logger.log 2>&1 &
